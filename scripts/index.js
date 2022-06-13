d25444c3bedcec65a9c281f9d8b6d62ef78461f8
Rimma27 <rimmaady@yandex.ru>
2022-06-13 23:30:25 +0300

    go popupa

--- scripts/index.js
+++ scripts/index.js
@@ -7,22 +7,28 @@ let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__button_close');
 let popup = document.querySelector('.popup');
 
+//                                     Функции
+
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     closePopup();
 }
-formElement.addEventListener('submit', formSubmitHandler);
 
 function openPopup() {
     popup.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }
-editButton.addEventListener('click', openPopup);
 
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
+
+
+//                                    Листнеры
+
+formElement.addEventListener('submit', formSubmitHandler);
+editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
