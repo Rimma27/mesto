1a6a5f1e97663530e3baad934e32da0051defa5f
Rimma27 <rimmaady@yandex.ru>
2022-06-27 18:57:14 +0300

    basket, popup-add

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,13 @@ let profileJob = document.querySelector('.profile__subtitle');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__button_type_close');
 let popup = document.querySelector('.popup');
+let addButton = document.querySelector('.profile__add-button');
+/*let likeButton = document.querySelector('.element__like');*/
+let popupAdd = document.querySelector('.popup-add');
+let placeInput = document.querySelector('.popup-add__item_type_place');
+let createButton = document.querySelector('.popup-add__button_type_create');
+let closeAddButton = document.querySelector('.popup-add__button_type_close');
+
 
 //                                     Функции
 
@@ -26,9 +33,27 @@ function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
+function openPopupAdd() {
+    popupAdd.classList.add('popup-add_opened');
+    nameInput.value = '';
+    placeInput.value = '';
+}
+
+function closePopupAdd() {
+    popupAdd.classList.remove('popup-add_opened');
+}
+
+
+
 
 //                                    Листнеры
 
 formElement.addEventListener('submit', formSubmitHandler);
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
+addButton.addEventListener('click', openPopupAdd);
+createButton.addEventListener('click', closePopupAdd);
+closeAddButton.addEventListener('click', closePopupAdd);
+/*likeButton.addEventListener('click', function(evt){
+    evt.target.classList.toggle('element__like_active');
+});*/
