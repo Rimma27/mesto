c59ead93863035f99c8f14dcf134a2f4db827164
Rimma27 <rimmaady@yandex.ru>
2022-06-28 00:16:49 +0300

    cards

--- scripts/index.js
+++ scripts/index.js
@@ -7,11 +7,59 @@ let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__button_type_close');
 let popup = document.querySelector('.popup');
 let addButton = document.querySelector('.profile__add-button');
-/*let likeButton = document.querySelector('.element__like');*/
 let popupAdd = document.querySelector('.popup-add');
 let placeInput = document.querySelector('.popup-add__item_type_place');
 let createButton = document.querySelector('.popup-add__button_type_create');
 let closeAddButton = document.querySelector('.popup-add__button_type_close');
+const popupPhoto = document.querySelector('#popup-photo');
+
+
+//                                  Карточки 
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const elementTemplate = document.querySelector('#element-template').content;
+const elements = document.querySelector('.elements');
+
+initialCards.forEach(function (item) {
+    const element = elementTemplate.querySelector('.element').cloneNode(true);
+    element.querySelector('.element__image').src = item.link;
+    element.querySelector('.element__image').alt = item.name;
+    element.querySelector('.element__title').textContent = item.name;
+    elements.append(element);
+    element.querySelector('.element__basket').addEventListener('click', function () {
+        element.remove();
+    });
+    element.querySelector('.element__like').addEventListener('click', function(evt){
+        evt.target.classList.toggle('element__like_active');
+    })
+
+});
+
 
 
 //                                     Функции
@@ -44,8 +92,6 @@ function closePopupAdd() {
 }
 
 
-
-
 //                                    Листнеры
 
 formElement.addEventListener('submit', formSubmitHandler);
@@ -54,6 +100,3 @@ closeButton.addEventListener('click', closePopup);
 addButton.addEventListener('click', openPopupAdd);
 createButton.addEventListener('click', closePopupAdd);
 closeAddButton.addEventListener('click', closePopupAdd);
-/*likeButton.addEventListener('click', function(evt){
-    evt.target.classList.toggle('element__like_active');
-});*/
